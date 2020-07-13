3b941651c12487d8c2ab23ae45509e3376e94d58
sagred <sagred17@gmail.com>
2020-07-13 21:54:47 +0530

    Updated

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = '<YOUR MONGODB URI>'
+const uri = process.env.MONGO_URI;
 mongoose.connect(uri,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 const connection = mongoose.connection;
 connection.once('open', () => {
